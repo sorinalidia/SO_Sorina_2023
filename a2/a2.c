4cc26d23e9b340a5678cbda8f31b73bf9c2e75bd
Tegla Sorina <sorinalidiategla@gmail.com>
2023-05-03 23:04:05 +0300

    first attempt

--- a2/a2.c
+++ a2/a2.c
@@ -2,13 +2,124 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pthread.h>
 #include "a2_helper.h"
 
-int main(){
-    init();
+#define NR_THREADS8 48
+#define NR_THREADS5 6
+#define NR_THREADS6 5
 
-    info(BEGIN, 1, 0);
-    
-    info(END, 1, 0);
+void* thread_function(void* arg)
+{
+    int thread_num = *(int*)arg;
+  
+    info(BEGIN, 6, thread_num);
+    if(thread_num==4)
+    {
+    info(END, 6, thread_num);
+    }
+    return NULL;
+}
+void* thread_function8(void* arg)
+{
+    int thread_num = *(int*)arg;
+    info(BEGIN, 8, thread_num);
+
+    info(END, 8, thread_num);
+    return NULL;
+}
+void* thread_function5(void* arg)
+{
+    int thread_num = *(int*)arg;
+    info(BEGIN, 5, thread_num);
+
+    info(END, 5, thread_num);
+    return NULL;
+}
+
+int main() {
+    init();
+    info(BEGIN,1,0);
+    pid_t pid2, pid3, pid4, pid5, pid6, pid7, pid8;
+   
+    pid2 = fork();
+    if (pid2 == 0) { // p2
+        info(BEGIN,2,0);
+        pid3 = fork();
+        if (pid3 == 0) { // p3
+            info(BEGIN,3,0);
+            pid4 = fork();
+            if (pid4 == 0) { // p4
+                info(BEGIN,4,0);
+                pid5 = fork();
+                if (pid5 == 0) { // p5
+                    info(BEGIN,5,0);
+                      pthread_t threads[NR_THREADS5];
+                    int thread_nums[NR_THREADS5];
+                    for (int i = 0; i < NR_THREADS5; i++) {
+                        thread_nums[i]=i+1;
+                        pthread_create(&threads[i], NULL, thread_function5, &thread_nums[i]);
+                    }
+                    for (int i = 0; i < NR_THREADS5; i++) {
+                        pthread_join(threads[i], NULL);
+                    }
+                    info(END,5,0);
+                    return 0;
+                }
+                waitpid(pid5,NULL,0);
+                pid6 = fork();
+                if (pid6 == 0) { // p6
+                    info(BEGIN,6,0);
+                    pthread_t threads[NR_THREADS6];
+                    int thread_nums[NR_THREADS6] = {1, 2, 3, 4, 5};
+                    for (int i = 0; i < NR_THREADS6; i++) {
+                        thread_nums[i]=i+1;
+                        pthread_create(&threads[i], NULL, thread_function, &thread_nums[i]);
+                    }
+                    for (int i = 0; i < NR_THREADS6; i++) {
+                        pthread_join(threads[i], NULL);
+                    }
+
+                    info(END,6,0);
+                    return 0;
+                }
+                waitpid(pid6,NULL,0);
+                pid7 = fork();
+                if (pid7 == 0) { // p7
+                    info(BEGIN,7,0);
+                    pid8 = fork();
+                    if (pid8 == 0) { // p8
+                        info(BEGIN,8,0);
+                        pthread_t threads[NR_THREADS8];
+                    int thread_nums[NR_THREADS8];
+                    for (int i = 0; i < NR_THREADS8; i++) {
+                        thread_nums[i]=i+1;
+                        pthread_create(&threads[i], NULL, thread_function8, &thread_nums[i]);
+                    }
+                    for (int i = 0; i < NR_THREADS8; i++) {
+                        pthread_join(threads[i], NULL);
+                    }
+                        info(END,8,0);
+                        return 0;
+                    }
+                    waitpid(pid8, NULL, 0);
+                    info(END,7,0);
+                    return 0;
+                }
+                waitpid(pid7, NULL, 0);
+                waitpid(pid4, NULL, 0);
+                info(END,4,0);
+                return 0;
+            }
+            waitpid(pid4, NULL, 0);
+            info(END,3,0);
+            return 0;
+        }
+        waitpid(pid3, NULL, 0);
+        info(END,2,0);
+        return 0;
+    }
+    waitpid(pid2, NULL, 0);
+    info(END,1,0);
     return 0;
 }
